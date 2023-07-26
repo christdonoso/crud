9e7b9ac4ebcccc3a675d94e911c18cc668ad8d49
Christian Donoso Aguilar <83191959+christdonoso@users.noreply.github.com>
2023-07-25 20:23:44 -0400

    Update settings.py

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+4mbmk)k=)byt)awy(wsdeg2c3nakzbb7!9hqhc!4andks04%4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['crud-app-0025.onrender.com']
+ALLOWED_HOSTS = ['crud-app-0025.onrender.com','*']
 
 
 # Application definition
